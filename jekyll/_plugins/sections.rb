d835ef72678e4e135e87e476cb6e062ae446d452
Mark Trapp <mark@marktrapp.com>
2014-01-06 20:45:57 -0800

    Refactor SectionGenerator to split out hashing method.

--- jekyll/_plugins/sections.rb
+++ jekyll/_plugins/sections.rb
@@ -12,26 +12,36 @@ module Jekyll
       SECTION_KEY_PLURAL = 'sections'
 
       def generate(site)
-        # Hash algorithm taken from Jekyl::Site::post_attr_hash
-        hash = Hash.new { |hsh, key| hsh[key] = Array.new }
-        site.posts.each do |post|
-          if post.data.has_key? SECTION_KEY
-            section = post.data.fetch(SECTION_KEY)
-
-            if site.config['section'] && site.config['section']['exclude']
-              exclude_categories = site.config['section']['exclude']
-            else
-              exclude_categories = []
-            end
-
-            unless exclude_categories.include? section
-              hash[section] << post
-            end
-          end
+        if site.config[SECTION_KEY] && site.config[SECTION_KEY]['exclude']
+          exclude = site.config[SECTION_KEY]['exclude']
+        else
+          exclude = []
         end
-        hash.values.map { |sortme| sortme.sort! { |a, b| b <=> a } }
+        site.config[SECTION_KEY_PLURAL] = post_key_hash(site, SECTION_KEY, exclude)
+      end
 
-       site.config[SECTION_KEY_PLURAL] = hash.sort
+      ##
+      # Generates a hash using a key from a post's front matter.
+      #
+      def post_key_hash(site, post_key, exclude)
+        # Build a hash map based on the specified post attribute ( post attr =>
+        # array of posts ) then sort each array in reverse order.
+        hash = Hash.new { |hsh, key| hsh[key] = Array.new }
+        site.posts.each do |p|
+          # Skip post if it doesn't have the correct key
+          next unless p.data.has_key? post_key
+
+          # Load the value for the key and check to see if it should be excluded
+          t = p.data.fetch(post_key)
+          next if exclude.include? t
+
+          # Add the post to the hash
+          hash[t] << p
+        end
+
+        # Sort the hash
+        hash.values.map { |sortme| sortme.sort! { |a, b| b <=> a } }
+        hash
       end
     end
   end
